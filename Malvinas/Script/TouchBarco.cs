2172856d584f78b67fda70e7b2085975c29cfe4f
joelkalt <joelkaltman@gmail.com>
2016-03-08 21:51:24 -0300

    Primer commit

---
+++ Malvinas/Script/TouchBarco.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchBarco : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount == 1)
+		{
+			Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+			Vector2 touchPos = new Vector2(wp.x, wp.y);
+			if (this.GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+			{
+				Destroy(this);
+			}
+		}
+	}
+}
